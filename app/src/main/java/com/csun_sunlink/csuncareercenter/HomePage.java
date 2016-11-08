92d8b290b9ddea53584ae14379114b2f6c5d51fe
olgakup <olga.kupchevskaya.812@my.csun.edu>
2016-11-07 18:19:44 -0800

    Home Page

---
+++ app/src/main/java/com/csun_sunlink/csuncareercenter/HomePage.java
@@ -0,0 +1,20 @@
+package com.csun_sunlink.csuncareercenter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by olgak on 11/7/16.
+ */
+
+public class HomePage extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        // Get Curent User: currentUser = ParseUser.getCurrentUser(); + check if the user !null
+
+
+    }
+
+}
