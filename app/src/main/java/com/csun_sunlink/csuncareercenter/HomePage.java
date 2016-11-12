3795aa939b4818df98169f0dbf949b5225cc182e
olgakup <olga.kupchevskaya.812@my.csun.edu>
2016-11-12 13:37:06 -0800

    Home Page edited, added icons for the menu

--- app/src/main/java/com/csun_sunlink/csuncareercenter/HomePage.java
+++ app/src/main/java/com/csun_sunlink/csuncareercenter/HomePage.java
@@ -4,6 +4,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,6 +24,13 @@ public class HomePage extends AppCompatActivity{
     private String userDegree = "Computer Science";
     private String screenHeader;
 
+    //Buttons:
+    ImageButton prflButton;
+    ImageButton srchButton;
+    ImageButton careerButton;
+    ImageButton rsrcButton;
+    ImageButton sttngButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -31,13 +40,61 @@ public class HomePage extends AppCompatActivity{
         TextView header = (TextView) findViewById(R.id.headerHomePage);
         header.setText(screenHeader);
 
+        //Profile picture:
+        //check if no profile picture on file use defualt:
+
         Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.defaultpicture);
         Bitmap circularBitmap = ProfileImageDrawable.getRoundedCornerBitmap(bitmap, 100);
 
         ImageView circularImageView = (ImageView)findViewById(R.id.imageView);
         circularImageView.setImageBitmap(circularBitmap);
 
-        ImageButton imgButton =(ImageButton)findViewById(R.id.profile_button);
+        //Main Menu Buttons:
+        //Profile:
+        prflButton =(ImageButton)findViewById(R.id.profile_button);
+        prflButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Intent intent = new Intent(HomePage.this, ProfileActivity.class);
+               // startActivity(intent);
+            }
+        });
+        //Search:
+        srchButton =(ImageButton)findViewById(R.id.search_button);
+        srchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent intent = new Intent(HomePage.this, SearchActivity.class);
+                // startActivity(intent);
+            }
+        });
+        //Career Center:
+        careerButton =(ImageButton)findViewById(R.id.careercenter_button);
+        careerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent intent = new Intent(HomePage.this, CareerCenterActivity.class);
+                // startActivity(intent);
+            }
+        });
+        //Resources:
+        rsrcButton = (ImageButton)findViewById(R.id.resources_button);
+        rsrcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent intent = new Intent(HomePage.this, ResourcesActivity.class);
+                // startActivity(intent);
+            }
+        });
+        //Settings:
+        sttngButton =(ImageButton)findViewById(R.id.settings_button);
+        sttngButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent intent = new Intent(HomePage.this, SettingsActivity.class);
+                // startActivity(intent);
+            }
+        });
 
 
     }
